54da842375cb8eadbb65cd756433281eabc9122a
Brandon Scott Brigance <brandon.brigance@gmail.com>
2016-09-07 05:52:37 +0000

    Update show and index listing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,14 @@
 class ArticlesController < ApplicationController
   
+  def index
+    @articles = Article.all
+  end
+  
   def show
+    @article = Article.find(params[:id])
+  end
+  
+  def edit
     @article = Article.find(params[:id])
   end
   
@@ -22,6 +30,15 @@ class ArticlesController < ApplicationController
     end
   end
   
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated"
+      redirect_to article_path(@article)
+    else
+      render :edit
+    end
+  end
   
   #private
   def article_params
