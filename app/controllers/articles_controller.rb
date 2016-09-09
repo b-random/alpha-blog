2e3f59bf80e81efbb3437390a32f9de9edbc1c25
Brandon Scott Brigance <brandon.brigance@gmail.com>
2016-09-09 05:51:48 +0000

    Fixed redundancies in articles_controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,15 +1,16 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
   
   def index
     @articles = Article.all
   end
   
   def show
-    @article = Article.find(params[:id])
+    set_article
   end
   
   def edit
-    @article = Article.find(params[:id])
+    set_article
   end
   
   def new
@@ -31,7 +32,7 @@ class ArticlesController < ApplicationController
   end
   
   def update
-    @article = Article.find(params[:id])
+    set_article
     if @article.update(article_params)
       flash[:notice] = "Article was successfully updated"
       redirect_to article_path(@article)
@@ -41,13 +42,18 @@ class ArticlesController < ApplicationController
   end
   
   def destroy
-    @article = Article.find(params[:id])
+    set_article
     @article.destroy
     flash[:notice] = "Article was successfully deleted"
     redirect_to articles_path
   end
   
   #private
+  
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
   def article_params
     params.require(:article).permit(:title, :description)
     #this is the secret method that stores the attributes we want to save and gets passed to the CREATE action.
