fc3c40a7b006311ac05faa349d9c17b92da2b017
Brandon Scott Brigance <brandon.brigance@gmail.com>
2016-09-22 06:39:41 +0000

    User profile, article display

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def gravatar_for(user, options = {size: 80}) #parameter 2 is the default size.  Override inline on user show page
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    size = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.username, class: "img-circle")
+    #this method can be found in Gravatar docs and is specific to Gravatar
+  end
 end
