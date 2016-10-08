3138206fb069a61720ad25cef2eba3bf752a042f
Brandon Scott Brigance <brandon.brigance@gmail.com>
2016-10-08 17:02:39 +0000

    Categories MVC + model/controller tests

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
   
   
   resources :users, except: [:new]
+  resources :categories, except: [:destroy]
   # except :new because :new URL is /signup
   
   # You can have the root of your site routed with "root"
